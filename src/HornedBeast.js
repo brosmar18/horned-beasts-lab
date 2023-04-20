a95cd6920fe30753171431353c64aa37de344bb5
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-20 10:42:51 -0500

    constructor function added to initialize the 'favorites' state. increaseFavorites function added to increase the value of favorites by 1

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,20 +1,35 @@
 import React from "react";
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: 0,
+    };
+  }
+
+  increaseFavorites = () => {
+    this.setState({ favorites: this.state.favorites + 1 });
+  };
+
   render() {
-    console.log(this.props);
     return (
       <>
         <section className="horned-beast">
           <h2 className="horned-beast__title">Beast Name: {this.props.title}</h2>
           <img
+            className="horned-beast__image"
             src={this.props.image_url}
             alt={this.props.title}
-            className="horned-beast__image"
+            onClick={this.increaseFavorites}
           />
-          <figcaption className="horned-beast__description">
-            Description: {this.props.description}
-          </figcaption>
+          <figcaption className="horned-beast__description">Description: {this.props.description}</figcaption>
+          <p className="horned-beast__favorites">
+            <span role="img" aria-label="heart">
+              ❤️
+            </span>{" "}
+            Favorites: {this.state.favorites}
+          </p>
         </section>
       </>
     );
