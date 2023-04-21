b54bf8371266c57de3fcb2fbe39cade3140d4425
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-21 09:48:10 -0500

    File names capitalized

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <>
+                <footer className="footer">
+                    <p className="footer__copyright">&copy; Bryan Garduno Gonzalez</p>
+                </footer>
+            </>
+        );
+    }
+}
+
+export default Footer;
