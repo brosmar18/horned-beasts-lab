473b613eb8ee7d98909efda19393efaa0ac8df26
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-24 21:43:03 -0500

    Form component added to App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Footer from './Footer';
 import Main from './Main';
 import SelectedBeast from './SelectedBeast'; // Import the SelectedBeast component
 import image_data from './data/data.json'; // Import data from a JSON file
+import { Form } from 'react-bootstrap';
+
 
 // Create the App component which is the main container for the application
 class App extends React.Component {
@@ -15,6 +17,7 @@ class App extends React.Component {
     this.state = {
       selectedBeast: null,
       showModal: false,
+      selectedHornCount: 0,
     };
   }
 
@@ -33,18 +36,31 @@ class App extends React.Component {
     });
   };
 
+  // Handle changes to seleected horn count
+  handleHornCountChange = (event) => {
+    this.setState({
+      selectedHornCount: event.target.value,
+    });
+  }
+
   // Render the App component, which includes the Header, Main, Footer, and SelectedBeast components
   render() {
-    // Find the selected beast object based on its title
     const selectedBeast = image_data.find(beast => beast.title === this.state.selectedBeast);
-
     return (
       <div className="App">
         <Header />
-        {/* Pass the onOpenModal function to the Main component as a prop */}
-        <Main onOpenModal={this.onOpenModal} />
+        <Form.Group controlId="formHornCount">
+          <Form.Label>Filter by Horn Count:</Form.Label>
+          <Form.Control as="select" onChange={this.handleHornCountChange}>
+            <option value={0}>All</option>
+            <option value={1}>One</option>
+            <option value={2}>Two</option>
+            <option value={3}>Three</option>
+            <option value={100}>100</option>
+          </Form.Control>
+        </Form.Group>
+        <Main onOpenModal={this.onOpenModal} selectedHornCount={this.state.selectedHornCount} />
         <Footer />
-        {/* Pass the selected beast object, onCloseModal function, and showModal state value to the SelectedBeast component */}
         <SelectedBeast
           beast={selectedBeast}
           onClose={this.onCloseModal}
